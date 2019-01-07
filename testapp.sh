c5a559e0f40e9834ebeb15ad8fba0701b87ff556
Christian Stussak <stussak@mfo.de>
2019-01-07 18:33:51 +0100

    Remove shell scripts that don't belong in this repository

--- testapp.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-SELFDIR=`dirname "$0"`
-SELFDIR=`cd "$SELFDIR" && pwd`
-
-exec "$SELFDIR/browser.sh" --verbose --verbose --verbose --verbose --integration --localhost --fullscreen --kiosk --always-on-top -l kiosk://testapp "$@"
