83c746a2417e595bb3ea11f921848e09c43d4583
Christian Stussak <stussak@mfo.de>
2019-01-18 18:53:00 +0100

    Remove additional scope around BrowserWindow initialization

--- src/js/main/appReady.js
+++ src/js/main/appReady.js
@@ -66,117 +66,115 @@ function appReady(settings, args, urlPrefix) {
     // either disable default menu (noop on macOS) or set custom menu (based on default)
     Menu.setApplicationMenu(args.menu && !args.kiosk ? extendMenu(Menu.getApplicationMenu()) : null);
 
-    {
-        const webprefs = {
-            plugins: true,
-            webSecurity: false,
-            experimentalFeatures: true,
-            allowRunningInsecureContent: true,
-            zoomFactor: args.zoom,
-            nodeIntegration: args.integration,
-        };
+    const webprefs = {
+        plugins: true,
+        webSecurity: false,
+        experimentalFeatures: true,
+        allowRunningInsecureContent: true,
+        zoomFactor: args.zoom,
+        nodeIntegration: args.integration,
+    };
 
-        if (args.preload)
-            webprefs.preload = path.resolve(args.preload);
+    if (args.preload)
+        webprefs.preload = path.resolve(args.preload);
 
-        const options = {
-            show: false,
-            frame: !args.transparent,
-            titleBarStyle: 'hidden-inset',
-            fullscreenable: true,
-            fullscreen: args.fullscreen,
-            kiosk: args.kiosk,
-            resizable: !args.transparent,
-            transparent: args.transparent,
-            alwaysOnTop: args["always-on-top"],
-            webPreferences: webprefs,
-            acceptFirstMouse: true,
-        };
-        if (process.platform === 'linux')
-            options.icon = path.resolve(__dirname, '../../../build/48x48.png');
+    const options = {
+        show: false,
+        frame: !args.transparent,
+        titleBarStyle: 'hidden-inset',
+        fullscreenable: true,
+        fullscreen: args.fullscreen,
+        kiosk: args.kiosk,
+        resizable: !args.transparent,
+        transparent: args.transparent,
+        alwaysOnTop: args["always-on-top"],
+        webPreferences: webprefs,
+        acceptFirstMouse: true,
+    };
+    if (process.platform === 'linux')
+        options.icon = path.resolve(__dirname, '../../../build/48x48.png');
 
-        mainWindow = new BrowserWindow(options);
+    mainWindow = new BrowserWindow(options);
 
-        process.on('SIGUSR1', () => mainWindow.webContents.toggleDevTools());
+    process.on('SIGUSR1', () => mainWindow.webContents.toggleDevTools());
 
-        mainWindow.webContents.on('new-window', event => event.preventDefault());
+    mainWindow.webContents.on('new-window', event => event.preventDefault());
 
-        // In the main process.
-        mainWindow.webContents.session.on('will-download', (event, item) => {
-            logger.info("Trying to Download: ");
-            logger.info(item.getFilename());
-            logger.info(item.getMimeType());
-            logger.info(item.getTotalBytes());
-            item.cancel(); // Nope... this is a kiosk!
-        });
+    // In the main process.
+    mainWindow.webContents.session.on('will-download', (event, item) => {
+        logger.info("Trying to Download: ");
+        logger.info(item.getFilename());
+        logger.info(item.getMimeType());
+        logger.info(item.getTotalBytes());
+        item.cancel(); // Nope... this is a kiosk!
+    });
 
-        mainWindow.once('ready-to-show', () => {
-            if (args.fullscreen)
-                fixFullscreenMode(mainWindow);
-            mainWindow.show();
-            mainWindow.focus();
-        });
+    mainWindow.once('ready-to-show', () => {
+        if (args.fullscreen)
+            fixFullscreenMode(mainWindow);
+        mainWindow.show();
+        mainWindow.focus();
+    });
 
-        /***
-         * Work around a Chrome bug that caches previously used zoom factors on a per page basis
-         * @see https://github.com/electron/electron/issues/10572
-         */
-        mainWindow.webContents.on('did-finish-load', () => mainWindow.webContents.setZoomFactor(args.zoom));
+    /***
+     * Work around a Chrome bug that caches previously used zoom factors on a per page basis
+     * @see https://github.com/electron/electron/issues/10572
+     */
+    mainWindow.webContents.on('did-finish-load', () => mainWindow.webContents.setZoomFactor(args.zoom));
 
-        // retry loading the page if it failed
-        mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription, validatedURL) => {
-            if (errorCode === -3 || errorCode === 0)
-                return;
+    // retry loading the page if it failed
+    mainWindow.webContents.on('did-fail-load', (event, errorCode, errorDescription, validatedURL) => {
+        if (errorCode === -3 || errorCode === 0)
+            return;
 
-            logger.warn(`Loading ${validatedURL} failed with Error ${errorCode}: ${errorDescription}`);
-            let errorMsgDiv = `
+        logger.warn(`Loading ${validatedURL} failed with Error ${errorCode}: ${errorDescription}`);
+        let errorMsgDiv = `
             <div style="position:absolute;top:0;left:0;width: 100%;color: white;background-color: black;">
                 Error ${errorCode}: ${errorDescription}<br />
                 URL: ${validatedURL}<br />
         `;
 
-            if (args.retry > 0)
-                errorMsgDiv += `Reloading in ${args.retry}s`;
+        if (args.retry > 0)
+            errorMsgDiv += `Reloading in ${args.retry}s`;
 
-            errorMsgDiv += `</div>`;
+        errorMsgDiv += `</div>`;
 
-            mainWindow.webContents.once('dom-ready', () => {
-                mainWindow.webContents.executeJavaScript(`document.body.innerHTML += ${JSON.stringify(errorMsgDiv)};`);
-                if (args.retry > 0) {
-                    mainWindow.webContents.executeJavaScript(`setTimeout(()=>window.location.reload(),${args.retry * 1000});`);
-                }
-            });
-        });
-
-        // Open the DevTools?
-        if (args.dev) {
-            mainWindow.openDevTools();
-        } // --remote-debugging-port=8315
-
-        // and load some URL?!
-        const partialUrl = (args._.length > 0) ? args._[0] : (args.url ? args.url : (args.serve ? 'index.html' : settings['home']));
-        const parseUrl = require('url').parse;
-        const parsedPartialUrl = parseUrl(partialUrl);
-        logger.debug('%o', parsedPartialUrl);
-        if (parsedPartialUrl.protocol === "kiosk:") {
-            switch (parsedPartialUrl.hostname) {
-                case 'home':
-                    mainWindow.loadURL('file://' + path.normalize(`${__dirname}/../../html/index.html`));
-                    break;
-                case 'testapp':
-                    mainWindow.loadURL('file://' + path.normalize(`${__dirname}/../../html/testapp.html`));
-                    break;
-                default:
-                    logger.error(`Unknown kiosk:// url: ${partialUrl}`);
-                    app.exit(-1);
+        mainWindow.webContents.once('dom-ready', () => {
+            mainWindow.webContents.executeJavaScript(`document.body.innerHTML += ${JSON.stringify(errorMsgDiv)};`);
+            if (args.retry > 0) {
+                mainWindow.webContents.executeJavaScript(`setTimeout(()=>window.location.reload(),${args.retry * 1000});`);
             }
-        } else {
-            const fullUrl = parsedPartialUrl.protocol === null ? (args.serve ? urlPrefix + partialUrl : `file://${path.resolve(partialUrl)}`) : partialUrl;
-            logger.debug(`urlPrefix: ${urlPrefix}`);
-            logger.debug(`partialUrl: ${partialUrl}`);
-            logger.debug(`Loading ${fullUrl}`);
-            mainWindow.loadURL(fullUrl);
+        });
+    });
+
+    // Open the DevTools?
+    if (args.dev) {
+        mainWindow.openDevTools();
+    } // --remote-debugging-port=8315
+
+    // and load some URL?!
+    const partialUrl = (args._.length > 0) ? args._[0] : (args.url ? args.url : (args.serve ? 'index.html' : settings['home']));
+    const parseUrl = require('url').parse;
+    const parsedPartialUrl = parseUrl(partialUrl);
+    logger.debug('%o', parsedPartialUrl);
+    if (parsedPartialUrl.protocol === "kiosk:") {
+        switch (parsedPartialUrl.hostname) {
+            case 'home':
+                mainWindow.loadURL('file://' + path.normalize(`${__dirname}/../../html/index.html`));
+                break;
+            case 'testapp':
+                mainWindow.loadURL('file://' + path.normalize(`${__dirname}/../../html/testapp.html`));
+                break;
+            default:
+                logger.error(`Unknown kiosk:// url: ${partialUrl}`);
+                app.exit(-1);
         }
+    } else {
+        const fullUrl = parsedPartialUrl.protocol === null ? (args.serve ? urlPrefix + partialUrl : `file://${path.resolve(partialUrl)}`) : partialUrl;
+        logger.debug(`urlPrefix: ${urlPrefix}`);
+        logger.debug(`partialUrl: ${partialUrl}`);
+        logger.debug(`Loading ${fullUrl}`);
+        mainWindow.loadURL(fullUrl);
     }
 
     return mainWindow;
