3850491d4512eb58d4b5b13465564b0200cdee48
Christian Stussak <stussak@mfo.de>
2020-03-25 13:13:27 +0100

    Factor out preload script that injects CSS into webFrames

--- src/js/renderer/disableSelection.js
+++ src/js/renderer/disableSelection.js
@@ -1,10 +1,4 @@
-const fsPromises = require('fs').promises;
 const path = require('path');
-const {webFrame} = require('electron');
+const insertCssIntoWebFrame = require('./insertCssIntoWebFrame.js');
 
-const domReady = new Promise(resolve => document.addEventListener("DOMContentLoaded", resolve));
-const cssPromise = fsPromises.readFile(path.join(__dirname, '../../css/disable-selection.css'), 'utf8');
-
-domReady
-    .then(() => cssPromise)
-    .then(css => webFrame.insertCSS(css, {cssOrigin: 'user'}));
+insertCssIntoWebFrame.fromFile(path.join(__dirname, '../../css/disable-selection.css'));
---
+++ src/js/renderer/domReady.js
@@ -0,0 +1,8 @@
+const domReady = new Promise(resolve => {
+    if (document.readyState === "complete" || document.readyState === "interactive")
+        resolve();
+    else
+        window.addEventListener("DOMContentLoaded", () => resolve());
+});
+
+module.exports = domReady;
