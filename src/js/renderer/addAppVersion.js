d9e9d4494cca355ed9c0b75cc868fd52c2e0caf2
Christian Stussak <stussak@mfo.de>
2020-11-24 13:41:00 +0100

    Fix app version info being unavailable when node integration is disabled

--- src/js/renderer/addAppVersion.js
+++ src/js/renderer/addAppVersion.js
@@ -1,3 +1,19 @@
 // Add the version of the kiosk browser to process.versions
-const {remote} = require('electron');
+const { remote } = require('electron');
+
 process.versions.app = remote.app.getVersion();
+const versions = process.versions;
+
+function isUndefined(v) {
+  return typeof v === 'undefined';
+}
+
+/**
+ * When run without node integration, Electron will remove the version info, but we want to keep it.
+ */
+function ensureVersions() {
+  window.process = isUndefined(window.process) ? {} : window.process;
+  window.process.versions = versions;
+}
+
+setTimeout(ensureVersions, 0); // needs to run as soon as all preload scripts ended
