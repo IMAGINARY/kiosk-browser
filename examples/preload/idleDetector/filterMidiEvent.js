7c1fae73963df88f3efd15d1e85a458fec10aad2
Christian Stussak <stussak@mfo.de>
2020-11-30 16:58:53 +0100

    Add idleDector example that discards repeated control change messages

--- examples/preload/idleDetector/filterMidiEvent.js
+++ examples/preload/idleDetector/filterMidiEvent.js
@@ -1,11 +1,41 @@
-function messageEventFilter(messageEvent) {
+/**
+ * Discard noteOff events.
+ */
+function filterNoteOff(messageEvent) {
   const command = messageEvent.data[0];
   if (command === 128) {
-    console.log("Ignoring noteOff MIDI message event.", messageEvent);
+    console.log('Ignoring noteOff MIDI message event.', messageEvent);
     return true;
   } else {
     return false;
   }
 }
 
-kioskBrowser.idleDetector.midi.messageEventFilters.add(messageEventFilter);
+/**
+ * Discard repeated control change messages with identical value.
+ */
+const controlChangess = [];
+
+function filterRepeatedControlChange(messageEvent) {
+  controlChangess[messageEvent.target.id] ??= [];
+  const controlChanges = controlChangess[messageEvent.target.id];
+  const data = messageEvent.data;
+  if (data[0] >> 4 === 0xB) { // this is a control change message
+    const controlId = (data[0] << 8) + data[1];
+    const changed = controlChanges[controlId] !== data[2];
+    controlChanges[controlId] = data[2];
+    if (changed) {
+      console.log('NOT ignoring MIDI control change event.', messageEvent);
+      return false;
+    } else {
+      console.log(
+        'Ignoring MIDI control change event because its value didn\'t change.',
+        messageEvent
+      );
+      return true;
+    }
+  }
+}
+
+kioskBrowser.idleDetector.midi.messageEventFilters.add(filterNoteOff);
+kioskBrowser.idleDetector.midi.messageEventFilters.add(filterRepeatedControlChange);
