fb167d6a564ace2274b2a14b84263076a36bde3d
Christian Stussak <stussak@mfo.de>
2020-12-02 10:09:27 +0100

    Fix transparent windows on Linux
    On Linux, Electron requires to add the Chromium command line switches
    --enable-transparent-visuals --disable-gpu to make transparency work.

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -93,6 +93,14 @@ async function main(args) {
     args['append-chrome-switch'].push({ key: 'host-rules', value: 'MAP * 127.0.0.1' });
   }
 
+  if (process.platform === 'linux' && args.transparent) {
+    // This is a workaround for
+    // https://github.com/electron/electron/blob/v11.0.0/docs/api/frameless-window.md#limitations
+    // TODO: Check regularly if the fix is still necessary
+    args['append-chrome-switch'].push({ key: '--enable-transparent-visuals', value: '' });
+    args['append-chrome-switch'].push({ key: '--disable-gpu', value: '' });
+  }
+
   applyChromiumCmdLine(args['use-minimal-chrome-cli'],
     args['append-chrome-switch'],
     args['append-chrome-argument']);
